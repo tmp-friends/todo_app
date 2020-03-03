54c6c092a724a48c430fa9d510ab5945be148579
t.terai <tomoya.terai01@gmail.com>
2020-03-03 23:56:33 +0900

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # get "/tasks" => "tasks#index"
+  # post "/tasks" => "tasks#create"
+  # get "/tasks/new" => "tasks#new"
+  # get "/tasks/:id/edit" => "tasks#edit"
+  # get "/tasks/:id/destroy" => "tasks#destroy"
+  # get "/tasks/:id" => "tasks#show"
+
 end
